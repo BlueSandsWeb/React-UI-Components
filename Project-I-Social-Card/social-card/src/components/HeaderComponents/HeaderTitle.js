b2dba34f870eca4084ea6497945982c09518cfda
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-02-11 15:55:57 -0800

    finished mvp for Social Card

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+  return (
+    <div className="HeaderTitle">
+      <h4 className="CardHeaderTitle">Lambda School</h4>
+      <a href="#" className="handle">@LambdaSchool</a><span>&#8226; 26 jan</span>
+    </div>
+  );
+}
+
+export default HeaderTitle;
