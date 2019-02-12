5804051773d288a86c1d1a3d22cb9af7f0485a55
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-02-11 16:47:41 -0800

    changed method for getting date to using moment.js instead of using vanilla javascript

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import './Header.css';
-
-let today = new Date();
-let dd = today.getDate();
-let mm = today.getMonth() + 1; //January is 0!
-let yyyy = today.getFullYear();
-
-if (dd < 10) {
-  dd = '0' + dd;
-}
-
-if (mm < 10) {
-  mm = '0' + mm;
-}
-
-today = mm + '/' + dd + '/' + yyyy;
-
+import moment from 'moment';
 
 function HeaderTitle() {
   return (
     <div className="HeaderTitle">
       <h4 className="CardHeaderTitle">Lambda School</h4>
-      <a href="#" className="handle">@LambdaSchool</a><span>&#8226; {today}</span>
+      <a href="#" className="handle">@LambdaSchool</a><span>&#8226; {moment().format("MMM Do")}</span>
     </div>
   );
 }
